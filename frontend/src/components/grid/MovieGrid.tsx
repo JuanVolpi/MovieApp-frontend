f4598727345c25ea67746c53d8b6668d9f873e5c
Juan Volpi <juan8volpi@gmail.com>
2025-06-13 14:17:59 +0100

    feat; movie grid component

---
+++ frontend/src/components/grid/MovieGrid.tsx
@@ -0,0 +1,30 @@
+import { Filme } from "@/types";
+import MovieCard from "../card/MovieCard";
+
+interface MovieGridProps {
+  filmes: Filme[];
+  onFilmeClick: (filme: Filme) => void;
+}
+
+export default function MovieGrid({ filmes, onFilmeClick }: MovieGridProps) {
+  console.log(filmes)
+  if (!filmes || filmes.length === 0) {
+    return (
+      <div className="text-center py-8">Nenhum filme encontrado.</div>
+    );
+  }
+
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-8">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+        {filmes.map((filme) => (
+          <MovieCard
+            key={filme.id}
+            filme={filme}
+            onClick={() => onFilmeClick(filme)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
