cc74b56aab399edbad27139c4778ea5a31d6b7e5
Juan Volpi <juan8volpi@gmail.com>
2025-06-14 07:08:44 +0100

    feat: add community route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/Filmes/MainPage";
+import CommunityPage from "./pages/community/CommunityPage";
 
 function App() {
   return (
     <Routes>
       <Route element={<MainPage />} path="/filmes" />
+      <Route element={<CommunityPage/>} path="/comunidade" />
     </Routes>
   );
 }
