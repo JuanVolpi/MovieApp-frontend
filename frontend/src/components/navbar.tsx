c753d2e8d39fd0ca73eebd177d9f24014d27ef2f
Juan Volpi <juan8volpi@gmail.com>
2025-06-15 19:04:56 +0100

    refactor: ajusta navbar com ícone e info de usuário

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -5,68 +5,102 @@ import {
   NavbarItem,
   Link,
   Button,
-} from "@heroui/react";
-import { useNavigate } from "react-router-dom";
-import ThemeSwitch from "./switch/ThemeSwitch";
+  addToast,
+  Tooltip
+} from '@heroui/react'
+import { useNavigate, useLocation } from 'react-router-dom'
+import ThemeSwitch from './switch/ThemeSwitch'
+import { useAuth } from '@/context/AuthContext'
+import { useEffect, useState } from 'react'
+import UserCard from './card/UserCard'
+import { ArrowLeftStartOnRectangleIcon } from '@heroicons/react/20/solid'
 
 export const MovieLogo = () => {
   return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
+    <svg fill='none' height='36' viewBox='0 0 32 32' width='36'>
       <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
+        clipRule='evenodd'
+        d='M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z'
+        fill='currentColor'
+        fillRule='evenodd'
       />
     </svg>
-  );
-};
+  )
+}
 
-export default function CustomNavbar() {
-  const navigate = useNavigate();
+export default function CustomNavbar () {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { user, logout } = useAuth()
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
 
   const handleLogout = () => {
-    // Simples simulação — pode integrar com auth depois
-    alert("Logout feito!");
-    navigate("/login");
-  };
+    logout()
+    navigate('/login')
+    addToast({
+      title: 'Logout efetuado',
+      color: 'primary',
+      description: 'Volte Sempre!',
+      variant: 'solid'
+    })
+  }
+
+  const linkClass = (path: string) =>
+    location.pathname === path ? 'text-primary font-semibold' : ''
 
   return (
-    <Navbar shouldHideOnScroll isBordered maxWidth="xl">
+    <Navbar shouldHideOnScroll isBordered maxWidth='xl'>
       {/* Logo à esquerda */}
-      <NavbarBrand as={Link} href="/filmes">
+      <NavbarBrand as={Link} href='/filmes'>
         <MovieLogo />
-        <p className="font-bold text-inherit ml-2">MovieApp</p>
+        <p className='font-bold text-inherit ml-2'>MovieApp</p>
       </NavbarBrand>
 
       {/* Menu central */}
-      <NavbarContent className="hidden sm:flex gap-6" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="/comunidade">
-            Comunidade
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="/minha-lista">
-            Minha Lista
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
+      {isClient && user && (
+        <NavbarContent className='hidden sm:flex gap-6' justify='center'>
+          <NavbarItem>
+            <Link href='/community' className={linkClass('/community')}>
+              Comunidade
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href='/minha-lista' className={linkClass('/minha-lista')}>
+              Minha Lista
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+      )}
 
       {/* Menu à direita */}
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="/perfil">Perfil</Link>
-        </NavbarItem>
-        <NavbarItem>
-        <ThemeSwitch></ThemeSwitch>
-        </NavbarItem>
-        <NavbarItem>
-          <Button color="danger" variant="flat" onClick={handleLogout}>
-            Logout
-          </Button>
-        </NavbarItem>
+      <NavbarContent justify='end'>
+        {isClient && user ? (
+          <>
+            <NavbarItem className='hidden lg:flex'>
+              <UserCard avatar={''} nome={user.username} email={user.email} />
+            </NavbarItem>
+            <NavbarItem>
+              <Tooltip content='Logout' placement='bottom' color='danger'>
+                <Button
+                  aria-label='Logout'
+                  isIconOnly
+                  startContent={
+                    <ArrowLeftStartOnRectangleIcon className='w-5' />
+                  }
+                  color='danger'
+                  variant='solid'
+                  onClick={handleLogout}
+                />
+              </Tooltip>
+            </NavbarItem>
+          </>
+        ) : null}
       </NavbarContent>
+      <ThemeSwitch />
     </Navbar>
-  );
+  )
 }
