8d2a219872f86ea65b899967b6430917c95948f9
Juan Volpi <juan8volpi@gmail.com>
2025-06-15 19:06:41 +0100

    feat: novo botão de alternância de tema com ícone

--- frontend/src/components/switch/ThemeSwitch.tsx
+++ frontend/src/components/switch/ThemeSwitch.tsx
@@ -1,28 +1,34 @@
-import { useTheme } from "@heroui/use-theme";
-import { Switch } from "@heroui/react";
-import SunIcon from "@heroicons/react/24/solid/SunIcon";
-import MoonIcon from "@heroicons/react/24/solid/MoonIcon";
+import { useTheme } from '@heroui/use-theme'
+import { Switch, Tooltip } from '@heroui/react'
+import SunIcon from '@heroicons/react/24/solid/SunIcon'
+import MoonIcon from '@heroicons/react/24/solid/MoonIcon'
 
-export default function ThemeSwitch() {
-  const { theme, setTheme } = useTheme();
+export default function ThemeSwitch () {
+  const { theme, setTheme } = useTheme()
 
   const toggleTheme = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
 
   return (
-    <Switch
-      isSelected={theme === "dark"}
-      onChange={toggleTheme}
-      size="lg"
-      color="secondary"
-      thumbIcon={({ isSelected, className }) =>
-        isSelected ? (
-          <MoonIcon className={className} />
-        ) : (
-          <SunIcon className={className} />
-        )
-      }
-    />
-  );
+    <Tooltip
+      content={`${theme === 'dark' ? 'Escuro' : 'Claro'}`}
+      placement='bottom'
+      color='secondary'
+    >
+      <Switch
+        isSelected={theme === 'dark'}
+        onChange={toggleTheme}
+        size='lg'
+        color='secondary'
+        thumbIcon={({ isSelected, className }) =>
+          isSelected ? (
+            <MoonIcon className={className} />
+          ) : (
+            <SunIcon className={className} />
+          )
+        }
+      />
+    </Tooltip>
+  )
 }
