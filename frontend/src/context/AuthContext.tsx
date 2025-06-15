6c7a721e6b3092f60c5af2b28a899c964615a30b
Juan Volpi <juan8volpi@gmail.com>
2025-06-15 19:06:59 +0100

    refactor: ajustes nos contextos de autenticação e tema

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,75 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { jwtDecode } from 'jwt-decode'
+import { User } from '@/types'
+
+interface AuthContextType {
+  user: User | null
+  token: string | null
+  loading: boolean
+  login: (token: string) => void
+  logout: () => void
+  isAuthenticated: () => boolean
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined)
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem('auth_token')
+  )
+  const [user, setUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (token) {
+      try {
+        const decoded: any = jwtDecode(token)
+        if (decoded.exp * 1000 < Date.now()) {
+          logout()
+        } else {
+          setUser({
+            id: parseInt(decoded.sub),
+            username: decoded.username,
+            avatar: decoded.avatar,
+            email: decoded.email
+          })
+          console.log(user)
+        }
+      } catch {
+        logout()
+      }
+    }
+    setLoading(false)
+  }, [token])
+
+  useEffect(() => {
+    console.log('Novo user carregado:', user)
+  }, [user])
+
+  const login = (newToken: string) => {
+    localStorage.setItem('auth_token', newToken)
+    setToken(newToken)
+  }
+
+  const logout = () => {
+    localStorage.removeItem('auth_token')
+    setToken(null)
+    setUser(null)
+  }
+
+  const isAuthenticated = () => !!token && !!user
+
+  return (
+    <AuthContext.Provider
+      value={{ user, token, login, logout, isAuthenticated, loading }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) throw new Error('useAuth must be used inside AuthProvider')
+  return context
+}
