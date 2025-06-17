bf769005c722cc06446e4e58c874ac2dbb64255d
Juan Volpi <juan8volpi@gmail.com>
2025-06-17 02:19:09 +0100

    refactor: ajusta estilização e comportamento do UserCard

--- frontend/src/components/card/UserCard.tsx
+++ frontend/src/components/card/UserCard.tsx
@@ -1,6 +1,7 @@
 // src/components/card/CardUser.tsx
 import { useAuth } from '@/context/AuthContext'
-import { User, Link, Card, CardBody } from '@heroui/react'
+import { User, Link, Card, CardBody, Tooltip } from '@heroui/react'
+import { div } from 'framer-motion/client'
 import { useNavigate } from 'react-router-dom'
 
 interface CardUserProps {
@@ -19,28 +20,32 @@ export default function CardUser ({ avatar, nome, email, href }: CardUserProps) 
   }
 
   return (
-    <Card
-      className={
-        'hover:scale-95 cursor-pointer px-4 py-2 flex justify-center items-center rounded-full'
-      }
-    >
-      <User
-        onClick={handleClick}
-        avatarProps={{
-          src: avatar ? avatar : `https://i.pravatar.cc/150?u=${user.id}`
-        }}
-        name={
-          href ? (
-            <Link href={href} className='text-base font-semibold'>
-              {nome}
-            </Link>
-          ) : (
-            nome
-          )
-        }
-        description={<span className='text-sm text-default-500'>{email}</span>}
-        className='max-w-full'
-      />
-    </Card>
+    <Tooltip content='Perfil' color='default' placement='bottom'>
+      <div className='hover:scale-95 transition-all duration-300 ease-in-out cursor - pointer'>
+        <Card
+          className={'px-4 py-2 flex justify-center items-center rounded-full'}
+        >
+          <User
+            onClick={handleClick}
+            avatarProps={{
+              src: avatar ? avatar : `https://i.pravatar.cc/150?u=${user.id}`
+            }}
+            name={
+              href ? (
+                <Link href={href} className='text-base font-semibold'>
+                  {nome}
+                </Link>
+              ) : (
+                nome
+              )
+            }
+            description={
+              <span className='text-sm text-default-500'>{email}</span>
+            }
+            className='max-w-full'
+          />
+        </Card>
+      </div>
+    </Tooltip>
   )
 }
