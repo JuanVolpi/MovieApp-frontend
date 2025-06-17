07b1ceb78d98e09c591e776fdbc67503426333c9
Juan Volpi <juan8volpi@gmail.com>
2025-06-17 09:25:58 +0100

    backend removido

--- Utilizador/extensions.py
+++
@@ -1,9 +0,0 @@
-# Utilizador/extensions.py
-
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-# Criamos as instâncias aqui, sem as ligar a nenhuma aplicação.
-# Elas funcionam como "proxies" que serão configurados mais tarde.
-db = SQLAlchemy()
-migrate = Migrate()
